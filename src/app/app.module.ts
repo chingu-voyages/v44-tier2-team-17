9cc0e283508d65286ca6a32ea87ad3dc53256733
draco-se <73908519+draco-se@users.noreply.github.com>
2023-05-09 15:50:59 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ArenaComponent } from './arena/arena.component';
+import { LeaderboardComponent } from './leaderboard/leaderboard.component';
+import { ConfigPanelComponent } from './config-panel/config-panel.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArenaComponent,
+    LeaderboardComponent,
+    ConfigPanelComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
