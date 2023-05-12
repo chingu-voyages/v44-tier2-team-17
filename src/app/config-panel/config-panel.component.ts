6100489b97f5006de9a5dada9253533325e33cd7
ivaa0202 <ivaa.0202@gmail.com>
2023-05-12 18:31:10 +0200

    fixed clearing form

--- src/app/config-panel/config-panel.component.ts
+++ src/app/config-panel/config-panel.component.ts
@@ -12,15 +12,22 @@ export class ConfigPanelComponent {
 
   startGameForm: FormGroup = new FormGroup({
     bot_name1: new FormControl("", [Validators.required]), //min length?
-    bot_name2: new FormControl("", [Validators.required])
+    bot_name2: new FormControl("", [Validators.required]),
+    boolean_val1: new FormControl("",[Validators.required]),
+    boolean_val2: new FormControl("",[Validators.required]),
+    boolean_op1: new FormControl("",[Validators.required]),
+    boolean_op2: new FormControl("",[Validators.required]),
+    direction1: new FormControl("", [Validators.required]),
+    direction2: new FormControl("",[Validators.required])
   })
 
   get botName1() { return this.startGameForm.get('bot_name1'); }
   get botName2() { return this.startGameForm.get('bot_name2'); }
 
-
   clearForm() {
-    this.startGameForm.reset();
+    this.startGameForm.reset({
+      boolean_val1: '', boolean_val2: '', boolean_op1: '', boolean_op2: '', direction1: '', direction2: ''
+    });
     this.uniqueName();
   }
 
@@ -36,8 +43,13 @@ export class ConfigPanelComponent {
 
     if (inp1.value === inp2.value && inp1.value !== '') {
       err.innerHTML = 'It has to be a uniqe name';
+      inp1.style.borderColor = 'red';
+      inp2.style.borderColor = 'red';
+
     } else {
       err.innerHTML = '';
+      inp1.style.borderColor = '';
+      inp2.style.borderColor = '';
     }
 
   }
